8707e837d21eb1827b70167a22d973b826589c0e
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2020-02-24 16:39:40 +0200

    add functional

---
+++ src/main/java/net/jms/rabbitmq/application/Task.java
@@ -0,0 +1,27 @@
+package net.jms.rabbitmq.application;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class Task {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        boolean durable = true;
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
+
+            String message = String.join(" ", argv);
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
