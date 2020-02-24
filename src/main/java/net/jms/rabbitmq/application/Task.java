9395c06f08325dcbb42326f4d795144ff6e99749
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2020-02-25 01:22:22 +0200

    add functional

--- src/main/java/net/jms/rabbitmq/application/Task.java
+++ src/main/java/net/jms/rabbitmq/application/Task.java
@@ -5,10 +5,20 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Task {
     private static final String TASK_QUEUE_NAME = "task_queue";
+    private static final String CHARSET = "UTF-8";
 
     public static void main(String[] argv) throws Exception {
+        AtomicInteger atomicInteger = new AtomicInteger();
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         boolean durable = true;
@@ -16,12 +26,26 @@ public class Task {
             Channel channel = connection.createChannel()) {
             channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
 
-            String message = String.join(" ", argv);
-
-            channel.basicPublish("", TASK_QUEUE_NAME,
-                    MessageProperties.PERSISTENT_TEXT_PLAIN,
-                    message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + message + "'");
+            ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
+            ScheduledFuture<?> future = executor.scheduleAtFixedRate(() -> {
+                String message = "task '" + atomicInteger.incrementAndGet() + "'";
+                try {
+                    channel.basicPublish("", TASK_QUEUE_NAME,
+                            MessageProperties.PERSISTENT_TEXT_PLAIN,
+                            message.getBytes(CHARSET));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(" [x] Sent " + message);
+            }, 500, 100, TimeUnit.MILLISECONDS);
+            try {
+                if (!executor.awaitTermination(30, TimeUnit.SECONDS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+            }
         }
+        System.out.println("Overall number of submitted tasks = " + atomicInteger);
     }
 }
--- src/main/java/net/jms/rabbitmq/application/Worker.java
+++ src/main/java/net/jms/rabbitmq/application/Worker.java
@@ -24,7 +24,7 @@ public class Worker {
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
 
-            System.out.println(" [x] Received '" + message + "'");
+            System.out.println(" [x] Received " + message + "");
             try {
                 doWork(message);
             } finally {
